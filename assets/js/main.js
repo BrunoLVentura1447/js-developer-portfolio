277337b5ac116d23436f26a0ce892cbd7380839e
BrunoLVentura1447 <brunoluizventura15@gmail.com>
2026-02-09 21:36:44 -0300

    adiciona seção de educação no perfil e atualiza estrutura do JSON

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,6 +64,17 @@ function updateProfessionalExperience(profileData) {
             `
         }).join('')
 }   
+
+function updateEducation(profileData) {
+    const education = document.getElementById('profile.education')
+    education.innerHTML = profileData.education.map(education => {
+        return `<li>
+                        <h3 class="title">${education.name}</h3>
+                        <p class="period">${education.period}</p>
+                        <p>${education.description}</p>
+                    </li>`
+    }).join('')
+}
     
 (async () => {
 
@@ -74,4 +85,5 @@ function updateProfessionalExperience(profileData) {
     updateLanguages(profileData)
     updatePortfolio(profileData)
     updateProfessionalExperience(profileData)
+    updateEducation(profileData)
 })()
